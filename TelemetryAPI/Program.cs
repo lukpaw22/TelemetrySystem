198224075d5bb7663f4256623f93a58d4fdd943d
lukpaw22 <240127324+lukpaw22@users.noreply.github.com>
2026-03-14 16:21:44 +0100

    Test API

--- TelemetryAPI/Program.cs
+++ TelemetryAPI/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.AspNetCore.Builder;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Hello World!");
+
+        app.Run("http://localhost:3000");
+    }
+}
