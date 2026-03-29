8ccdf365e9e7fe3b24fbcf47889ce6e51c0691c1
FilipO <filip.osobka@student.wsb.edu.pl>
2026-03-29 21:01:39 +0200

    small changes in program.cs and completed rebuilding RabbitMqConsumer.cs

--- TelemetryWorker/Services/RabbitMqConsumer.cs
+++ TelemetryWorker/Services/RabbitMqConsumer.cs
@@ -48,13 +48,41 @@ namespace TelemetryWorker.Services
                 try
                 {
                     Console.WriteLine($"Odebrano: {message}");
+                    await ProcessMessageAsync(message, cancellationToken);
+                    await _channel.BasicAckAsync(
+                        deliveryTag: ea.DeliveryTag,
+                        multiple: false,
+                        cancellationToken: cancellationToken);
                 }
-                catch
+                catch(Exception ex)
                 {
-
+                    Console.WriteLine($"Błąd: {ex.Message}");
+                    await _channel.BasicAckAsync(
+                        deliveryTag: ea.DeliveryTag,
+                        multiple: false,
+                        requeue: false,
+                        cancellationToken: cancellationToken);
                 }
             };
+            await _channel.BasicConsumeAsync(
+                queue: "telemetry",
+                autoAck: false,
+                consumer: consumer,
+                cancellationToken: cancellationToken);
 
+            Console.WriteLine("Async nasłuchiwanie kolejki...");
+        }
+        private async Task ProcessMessageAsync(string message, CancellationToken ct)
+        {
+            await Task.Delay(50, ct);
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if( _channel != null)
+                await _channel.CloseAsync();
+            if(_connection != null)
+                await _connection.CloseAsync();
         }
     }
 }
