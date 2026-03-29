3f0ccd8a75743a6bbdb43be87bf5f7e20e50dd9c
FilipO <filip.osobka@student.wsb.edu.pl>
2026-03-29 20:08:13 +0200

    Part 2 - services and main

---
+++ TelemetryWorker/Services/InfluxService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using InfluxDB.Client;
+using InfluxDB.Client.Writes;
+using TelemetryWorker.Models;
+
+namespace TelemetryWorker.Services
+{
+    public class InfluxService
+    {
+        private readonly InfluxxDBClient _client;
+        private readonly string _bucket = "telemetry";
+        private readonly string _org = "my-org";
+
+        public InfluxService(string url, string token)
+        {
+            _client = InfluxDBClientFactory.Create(url, token);
+        }
+        public async Task WriteAsync(TelemetryMessage msg)
+        {
+            var point = PointData
+            .Measurment("temperature")
+            .Tag("room", msg.Room)
+            .Field("temperature")
+            .Timestamp(msg.Timestamp, WritePrecision.Ns);
+
+            using var writeApi = _client.GetWriteApiAsync();
+            await writeApi.WritePointAsync(point, _bucket,  _org);
+        }
+    }
+}
---
+++ TelemetryWorker/Services/TelemetryProcessor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TelemetryWorker.Models;
+using TelemetryWorker.Validation;
+
+namespace TelemetryWorker.Services
+{
+    public class TelemetryProcessor
+    {
+        private readonly TelemetryValidator _validator;
+        private readonly InfluxService _influx;
+
+        public TelemetryProcessor(TelemetryValidator validator, InfluxService inlux)
+        {
+            _validator = validator;
+            _influx = inlux;
+        }
+
+        public async Task ProcessAsync(TelemetryMessage msg)
+        {
+            if (!_validator.IsValid(msg))
+                throw new Exception("Validation failed");
+            if (!_validator.ValidateHash(msg))
+                throw new Exception("Invalid hash");
+            await _influx.WriteAsync(msg);
+        }
+    }
+}
